2b835cdc37726ae80a49b7836b4a32f4ab05fc3d
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2020-11-14 14:05:05 +0500

    Add files via upload

---
+++ Middlestr.vb
@@ -0,0 +1,31 @@
+﻿Module Middlestr
+
+    Sub Main()
+        'Decleration
+        Dim x, middleStr As String
+        Dim sp1, sp2 As Integer
+
+        'Initialization
+        sp1 = 0
+        sp2 = 0
+        x = ""
+        middleStr = ""
+
+        'Input or 3 names
+        Console.Write("input 3 names: ")
+
+        'Working for middle string
+        x = Console.ReadLine()
+        sp1 = InStr(x, " ")
+        sp2 = InStr(sp1 + 1, x, " ")
+        middleStr = Mid(x, sp1 + 1, sp2 - sp1)
+
+        'Output of middle string
+        Console.WriteLine(middleStr)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
