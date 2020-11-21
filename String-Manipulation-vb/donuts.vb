888408da249db8527c44774a5656dbd3ac6c533e
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2020-11-21 16:55:42 +0500

    Add files via upload

---
+++ String-Manipulation-vb/donuts.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer = 0
+
+        Console.Write("Enter number of donuts: ")
+        x = Console.ReadLine()
+        If x < 10 Then
+            Console.WriteLine("Number of donuts: " & Str(x))
+        Else
+            Console.WriteLine("number of donuts: many")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
