95e9517d478fa9b97dd467763d6eda4c5e04625d
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2020-11-21 16:39:42 +0500

    Add files via upload

---
+++ String-Manipulation-vb/both_ends.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s As String = ""
+
+        Console.Write("Input a string: ")
+        s = Console.ReadLine()
+        If Len(s) > 4 Then
+            Console.WriteLine("The output string is: " & Left(s, 2) & Right(s, 2))
+        Else
+            Console.WriteLine("The output string is: " & s)
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
