0aa6248366573183d45739b16c40f384228c51dc
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2020-11-21 14:26:24 +0500

    Add files via upload

---
+++ String-Manipulation-vb/Middlestr.vb
@@ -0,0 +1,33 @@
+﻿Module Middlestr
+
+    Sub Main()
+        'Declaration and Initialization
+        Dim x As String = ""
+        Dim vowel As String = ""
+        Dim vowelcount As Integer = 0
+        Dim z As Integer
+        Dim y As Integer
+        Dim mychar As Char
+
+
+        'Working to find vowels in the string
+        vowel = "aeiou"
+        Console.Write("Enter a string: ")
+        x = Console.ReadLine()
+        For z = 1 To Len(x)
+            For y = 1 To 5
+                mychar = Mid(x, z, 1)
+                If mychar = Mid(vowel, y, 1) Then
+                    vowelcount = vowelcount + 1
+
+                End If
+            Next
+        Next
+        Console.WriteLine("The number of vowels in the string is: " & vowelcount)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
---
+++ String-Manipulation-vb/Verbing.vb
@@ -0,0 +1,19 @@
+﻿Module Verbing
+
+    Sub Main()
+        Dim mystr As String = ""
+        Console.Write("Input a string: ")
+        mystr = Console.ReadLine()
+        If Len(mystr) < 3 Then
+            If LCase(Right(mystr, 3)) <> "ing" Then
+                mystr = mystr + "ing"
+            Else
+                mystr = mystr + "ly"
+            End If
+        End If
+        Console.WriteLine("The output string is: " & mystr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
