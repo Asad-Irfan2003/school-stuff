8501d50ffe43402e837e7e7b0bb7b3e2cd8e33e5
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2020-11-23 22:28:46 +0500

    Add files via upload

---
+++ String-Manipulation-vb/match_ends.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s() As String = {"charismatic", "happiness", "agenda", "for", "to", "so", "demand", "greeting"}
+        Dim count As Integer = 0
+        Dim strlength As String = ""
+        Dim fchar As Char = ""
+        Dim lchar As Char = ""
+        Dim x As Integer = 0
+
+        For x = 0 To 7
+            fchar = Left(s(x), 1)
+            lchar = Right(s(x), 1)
+            strlength = Len(s(x))
+            If fchar = lchar And strlength >= 2 Then
+                count = count + 1
+            End If
+        Next
+        Console.WriteLine("The output string is: " & count)
+        Console.ReadKey()
+    End Sub
+
+End Module
