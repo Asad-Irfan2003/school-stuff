b3cb29b3eb793596b6df939759144159ba7e234f
Asad-Irfan2003 <73904908+Asad-Irfan2003@users.noreply.github.com>
2024-05-12 17:01:48 +0500

    Create 1-exception.cpp

---
+++ OOP/exception_handling/1-exception.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+//Exception: A runtime abnormal condition or anomalies can a program occurs during execution
+//It may cause the program to stop working
+
+//Example: Division by zero
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    float div=a/b;
+    cout<<div<<endl;
+}
+
+
+//Solution of Exception: Use exception handling
